d9be8f256556aacca507c648983042a0b197d90c
Kuto <86799382+Kutopus@users.noreply.github.com>
2021-11-17 15:26:29 -0300

    Update BITPREÇO.py

--- BITPREÇO.py
+++ BITPREÇO.py
@@ -15,7 +15,7 @@ x = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 values = []
 z = 0
 
-while z != 10:
+while z != 10: # CHANGE HOW MANY TIMES YOU WANT TO REQUEST THE PRICES
     for tag in divs:
         print(strftime("%d-%m-%Y %H:%M:%S", gmtime()), ": ", tag.text.strip())
         tag = str(tag.text)
@@ -23,7 +23,7 @@ while z != 10:
         tag = tag.replace(",", "")
         tag = float(tag)
         values.append(tag)
-        time.sleep(0.1)
+        time.sleep(0.1) # CHANGE THE TIME OF EACH REQUEST HERE (IN SECONDS)
         z += 1
 
 plt.plot(x, values)
